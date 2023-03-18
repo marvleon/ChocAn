75cc705dcc4c8229aa8193b79bfb2b2c16bcfdfe
Yyyyyyuki1021 <92616380+Yyyyyyuki1021@users.noreply.github.com>
2023-03-18 19:16:47 +0900

    Create service_ledger.hpp

---
+++ service_ledger.hpp
@@ -0,0 +1,27 @@
+#ifndef SERVICE_LEDGER_HPP
+#define SERVICE_LEDGER_HPP
+
+#include "Provider_Directory.hpp"
+#include "Service_Record.hpp"
+#include <map>
+#include <vector>
+#include <utility>
+
+class Service_Ledger {
+    public:
+        Service_Ledger();
+        ~Service_Ledger();
+        void load();
+        void save();
+        void init(nlohmann::json);
+        void new_transaction(Service_Record &);
+        void generate_APR(Provider_Directory &);
+        void generate_EFT(Provider_Directory &); 
+        bool in_ledger(std::string);
+        std::string vec_to_string(const std::string&);
+
+    private:
+        std::unordered_map<std::string, std::vector<Service_Record>> ledger;
+};
+
+#endif // SERVICE_LEDGER_HPP
